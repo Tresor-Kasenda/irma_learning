b89caac62afefb1efe6de56e26a5b95c1c6a8b39
Tresor kasenda <tresorkasendat@gmail.com>
2025-02-24 11:59:03 +0200

    feat: add trainings table migration; include foreign key to master_class and various nullable fields

---
+++ database/migrations/2025_02_24_092301_create_trainings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('master_class_id')->constrained();
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->string('path')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->longText('description')->nullable();
+            $table->boolean('is_completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainings');
+    }
+};
