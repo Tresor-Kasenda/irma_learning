a7639e69bd414e42830bb703f37fc873ab1868b4
Tresor kasenda <tresorkasendat@gmail.com>
2025-03-01 17:47:09 +0200

    add exam submission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -34,6 +35,7 @@ final class AppServiceProvider extends ServiceProvider
         $this->shouldBeStrict();
         $this->configureDates();
         $this->configurePasswordValidation();
+        Schema::defaultStringLength(191);
     }
 
     public function configureCommands(): void
@@ -50,13 +52,8 @@ final class AppServiceProvider extends ServiceProvider
 
     public function shouldBeStrict(): void
     {
-        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::shouldBeStrict(!$this->app->isProduction());
         Model::unguard();
-    }
-
-    public function configureUrl(): void
-    {
-        URL::forceScheme('https');
     }
 
     /**
@@ -72,6 +69,11 @@ final class AppServiceProvider extends ServiceProvider
      */
     private function configurePasswordValidation(): void
     {
-        Password::defaults(fn () => $this->app->isProduction() ? Password::min(8)->uncompromised() : null);
+        Password::defaults(fn() => $this->app->isProduction() ? Password::min(8)->uncompromised() : null);
+    }
+
+    public function configureUrl(): void
+    {
+        URL::forceScheme('https');
     }
 }
