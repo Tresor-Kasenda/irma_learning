af6ecd6ddeb3c994ee1a9f58ad0dc07a496f6477
Tresor kasenda <tresorkasendat@gmail.com>
2025-03-01 13:09:45 +0200

    add exam submission

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,7 +44,7 @@ final class AdminPanelProvider extends PanelProvider
             ->favicon(asset('images/irma-logo-base.svg'))
             ->brandName('Certification')
             ->brandLogo(asset('images/irma-logo-base.svg'))
-            ->brandLogoHeight('5rem')
+            ->brandLogoHeight('3rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
