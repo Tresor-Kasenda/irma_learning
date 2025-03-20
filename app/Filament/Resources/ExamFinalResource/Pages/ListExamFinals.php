f80bb750700ef765967e66ddd1cedb4b87daa1e8
Tresor kasenda <tresorkasendat@gmail.com>
2025-03-20 22:36:01 +0200

    Refactor ListExamFinals; remove header actions and update manifest file for asset changes

--- app/Filament/Resources/ExamFinalResource/Pages/ListExamFinals.php
+++ app/Filament/Resources/ExamFinalResource/Pages/ListExamFinals.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\ExamFinalResource\Pages;
 
 use App\Filament\Resources\ExamFinalResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListExamFinals extends ListRecords
 {
     protected static string $resource = ExamFinalResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
